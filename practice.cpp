6949021cecff55e516a3385437b4d713e0c99218
muhammad junaid khan <junaid6078@gmail.com>
2020-05-15 15:59:34 +0500

    add practice file

---
+++ practice.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+int input;
+{
+	cout<<"enter the number and will show the table of thr number: ";
+	cin>>input;
+}
+{
+	cout<<input<<"*"<<"1"<<"="<<input*1<<endl;
+	cout<<input<<"*"<<"2"<<"="<<input*2<<endl;
+	cout<<input<<"*"<<"3"<<"="<<input*3<<endl;
+	cout<<input<<"*"<<"4"<<"="<<input*4<<endl;
+	cout<<input<<"*"<<"5"<<"="<<input*5<<endl;
+	cout<<input<<"*"<<"6"<<"="<<input*6<<endl;
+	cout<<input<<"*"<<"7"<<"="<<input*7<<endl;
+	cout<<input<<"*"<<"8"<<"="<<input*8<<endl;
+	cout<<input<<"*"<<"9"<<"="<<input*9<<endl;
+	cout<<input<<"*"<<"10"<<"="<<input*10<<endl;
+	
+	
+}
+	return 0;
+
+}
+
