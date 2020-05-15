4ec137a995adf0ebc7452decbc2222866f956026
muhammad junaid khan <junaid6078@gmail.com>
2020-05-15 15:59:05 +0500

    add lab5

---
+++ lab5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int number;
+
+	cout<<"enter a number: ";
+	cin>>number;
+	{
+	for (int i=1;i<=10;i++)
+	cout<<number<<"*"<<i<<"="<<number*i<<endl;
+	}
+	return 0;
+}
